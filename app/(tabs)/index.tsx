292668d7d0bf4158773aa5728bda6d9729af5524
AbdurrahimOzagac <abdozagac@gmail.com>
2025-10-31 19:15:11 +0300

    Text properties added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function HomeScreen() {
+export default function HeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World 👋</Text>
+      <Text style={styles.text}> Merhaba {isim} !</Text>
     </View>
   );
 }
+
+let isim: string = "Abdurrahim";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(58, 146, 138, 1)',
   },
   text: {
-    fontSize: 24,
+    fontSize: 34,
     fontWeight: 'bold',
+    color: 'rgba(255, 196, 0, 1)',
+    textAlign: 'center',
   },
 });
